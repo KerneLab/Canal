feb07823d7840c9e2dab38560ad1e37ee5544500
核心力量 <kernelforce@gmail.com>
2016-09-26 19:17:26 +0800

    The project has been wrapped.

--- src/canal.js
+++ src/canal.js
@@ -1,83 +1,160 @@
 /**
- * A FP style framework of data manipulate in javascript.
+ * An FP style framework of data manipulate in javascript.
  */
-function Canal()
+(function()
 {
-	this.data = null;
-	this.source = null;
-	this.operator = null;
+	var root = (typeof global === "object" && global) || this;
 
-	switch (arguments.length)
+	function Canal()
 	{
-		case 0:
-			break;
+		this.source = [];
 
-		case 1: // data
-			this.data = new Canal.DataSource(arguments[0]);
-			break;
+		this.index = 0;
 
-		case 2: // source,intermediate
-			this.source = arguments[0];
-			this.operator = arguments[1];
-			break;
+		this.operator = function(d)
+		{
+			return d;
+		};
+
+		switch (arguments.length)
+		{
+			case 0:
+				break;
+
+			case 1: // Array
+				// TODO direct source
+				this.source.push(new CanalSource(arguments[0]));
+				break;
+
+			case 2: // source,intermediate
+				this.source.push(arguments[0]);
+				this.operator = arguments[1];
+				break;
+		}
 	}
-}
 
-// /////////////////Basic////////////////
+	// /////////////////Basic////////////////
 
-Canal.prototype.next = function()
-{
-	if (this.data != null)
+	Canal.prototype.gather = function()
 	{
-		return this.data.next();
-	}
-	else
+		// TODO
+	};
+
+	Canal.prototype.iterator = function()
 	{
-		return this.operator(this.source.next());
-	}
-};
+		return new CanalIterator(this);
+	};
 
-// /////////////////Convert////////////////
+	// /////////////////Convert////////////////
 
-Canal.prototype.map = function(f)
-{
-	return new Canal(this, f);
-};
-
-// /////////////////Taking////////////////
-
-Canal.prototype.collect = function() // Array
-{
-	var result = [];
-	while ((v = this.next()) !== undefined)
+	Canal.prototype.map = function(f)
 	{
-		result.push(v);
-	}
-	return result;
-};
+		return new Canal(this, f);
+	};
 
-Canal.DataSource = function(data)
-{
-	this.data = data;
-	this.index = 0;
-};
+	// /////////////////Taking////////////////
 
-Canal.DataSource.prototype.next = function()
-{
-	if (this.index < this.data.length)
+	Canal.prototype.collect = function() // Array
 	{
-		return this.data[this.index++];
-	}
-	else
-	{
-		return undefined;
-	}
-};
+		var result = [];
+		var iter = this.iterator();
 
-Canal.derive = function(sub)
-{
-	var hyp = arguments.length > 1 ? arguments[1] : new Canal();
-	sub.prototype = hyp;
-	sub.prototype.constructor = sub;
-	sub.prototype.hyper = hyp;
-};
+		while (iter.hasNext())
+		{
+			result.push(iter.next());
+		}
+		return result;
+	};
+
+	// /////////////////Class////////////////
+
+	function CanalIterator(canal)
+	{
+		this.canal = canal;
+		this.index = 0;
+		this.iter = undefined;
+		this.value = undefined;
+	}
+
+	CanalIterator.prototype.hasNext = function()
+	{
+		while (this.index < this.canal.source.length && this.index >= 0)
+		{
+			if (this.iter === undefined)
+			{
+				this.iter = this.canal.source[this.index].iterator();
+			}
+
+			if (this.iter.hasNext())
+			{
+				this.value = this.iter.next();
+				return true;
+			}
+			else
+			{
+				this.index++;
+				this.iter = undefined;
+			}
+		}
+		return false;
+	};
+
+	CanalIterator.prototype.next = function()
+	{
+		return this.canal.operator(this.value);
+	};
+
+	// ============================
+
+	function SourceIterator(source)
+	{
+		this.source = source;
+		this.index = 0;
+		this.value = undefined;
+	}
+
+	SourceIterator.prototype.hasNext = function() // Boolean
+	{
+		this.value = this.source.get(this.index++);
+		return this.value !== undefined;
+	};
+
+	SourceIterator.prototype.next = function() // Object
+	{
+		return this.value;
+	};
+
+	// ============================
+
+	function CanalSource(data)
+	{
+		this.data = data;
+	}
+
+	CanalSource.prototype.iterator = function()
+	{
+		return new SourceIterator(this);
+	};
+
+	CanalSource.prototype.get = function(index)
+	{
+		if (index < this.data.length)
+		{
+			return this.data[index];
+		}
+		else
+		{
+			return undefined;
+		}
+	};
+
+	// Canal.derive = function(sub)
+	// {
+	// var hyp = arguments.length > 1 ? arguments[1] : new Canal();
+	// sub.prototype = hyp;
+	// sub.prototype.constructor = sub;
+	// sub.prototype.hyper = hyp;
+	// };
+
+	root.Canal = Canal;
+}.call(this));
