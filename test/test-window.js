f09f28c8c2e8e7be03ccddb5c6648267b3473443
核心力量 <kernelforce@gmail.com>
2016-11-24 20:04:33 +0800

    Update the test case.
    Which demostrates the sliding window by using between expression.

--- test/test-window.js
+++ test/test-window.js
@@ -23,13 +23,17 @@ QUnit.test("window()", function(assert)
 			.partBy(function(d){return d.grp;})
 			.orderBy(function(d){return d.sex;}),
 		Canal.item(sum(function(d){return d.sal;}))
-			.partBy(function(d){return d.grp;}, function(d){return d.sex;})
+			.partBy(function(d){return d.grp;}, function(d){return d.sex;}),
+		Canal.item(sum(function(d){return d.sal;}))
+			.partBy(function(d){return d.grp;})
+			.orderBy(function(d){return d.sal;})
+			.between(null,0)  // From the very first row to current row
 	).collect();
 
 	assert.propEqual(result, [
-		[{"id":1,"sex":1,"grp":0,"sal":3032}, 1, 3032],
-		[{"id":3,"sex":2,"grp":0,"sal":2545}, 4, 2545],
-		[{"id":2,"sex":1,"grp":1,"sal":2153}, 6, 4047],
-		[{"id":4,"sex":1,"grp":1,"sal":1894}, 6, 4047]
+	    [{"id":3,"sex":2,"grp":0,"sal":2545}, 4, 2545, 2545],
+		[{"id":1,"sex":1,"grp":0,"sal":3032}, 1, 3032, 5577],
+		[{"id":4,"sex":1,"grp":1,"sal":1894}, 6, 4047, 1894],
+		[{"id":2,"sex":1,"grp":1,"sal":2153}, 6, 4047, 4047]
 	]);
 });
