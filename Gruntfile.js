b6f8d21cb5a585a58448d67a820bc1bde37d3d82
核心力量 <kernelforce@gmail.com>
2017-05-18 19:46:29 +0800

    Remove uglify task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,6 +23,6 @@ module.exports = function(grunt) {
 
   grunt.registerTask('test', ['qunit']);
 
-  grunt.registerTask('default', ['qunit', 'uglify']);
+  grunt.registerTask('default', ['qunit'/*, 'uglify'*/]);
 
 };
