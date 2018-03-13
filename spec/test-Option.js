fd3c129d69c6a1dad892f51d1e5c678c90a0525f
核心力量 <kernelforce@gmail.com>
2018-03-13 22:49:39 +0800

    Add Option.or(val), orNull() and given() test cases.

--- spec/test-Option.js
+++ spec/test-Option.js
@@ -1,23 +1,34 @@
 var Canal = require('../canal.js');
 var expect = require("expect.js");
 
-describe("Test Option", function(){
-
-it("Some()", function()
+describe("Test Option", function()
 {
-	var result = Canal.Some(1).map(function(d)
+	it("Some()", function()
 	{
-		return d + 1;
-	}).collect();
+		var result = Canal.Some(1);
 
-	expect(result).to.eql([ 2 ]);
-});
+		expect(result.map(function(d)
+		{
+			return d + 1;
+		}).collect()).to.eql([ 2 ]);
 
-it("None()", function()
-{
-	var result = Canal.None().collect();
+		expect(result.or(2)).to.eql(1);
 
-	expect(result).to.eql([]);
-});
+		expect(result.given()).to.be(true);
 
+		expect(result.orNull()).to.be(1);
+	});
+
+	it("None()", function()
+	{
+		var result = Canal.None();
+
+		expect(result.collect()).to.eql([]);
+
+		expect(result.or(2)).to.eql(2);
+
+		expect(result.given()).to.be(false);
+
+		expect(result.orNull()).to.be(null);
+	});
 });
