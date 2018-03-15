61e1b1c092cc86b56bbfdcd4ef78aaebe4eea1c3
核心力量 <kernelforce@gmail.com>
2018-03-15 23:19:00 +0800

    Refine the export method.

--- canal.js
+++ canal.js
@@ -5,8 +5,7 @@
  */
 (function(global, factory)
 {
-	typeof exports === "object" && typeof module !== "undefined" ? (module.exports = factory()) //
-	: typeof define === "function" && define.amd ? define(factory) : (global.Canal = factory());
+	typeof exports === "object" && typeof module !== "undefined" ? (module.exports = factory()) : typeof define === "function" && define.amd ? define(factory) : (global.Canal = factory());
 }(this, (function()
 {
 	"use strict";
